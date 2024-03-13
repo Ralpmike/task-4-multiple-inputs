cf468d15e38185f6bf8a72ea559bee06a20dad31
Rapahel Michael <ralphmike996@gmail.com>
2024-03-13 11:45:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react';
 
 function App() {
+
+  
+  const [userDetails, setUserDetails] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    src: ""
+  })
+
+  const [image, setImage] = useState()
+  console.log(image)
+
+ const  handleChange = (e) => {
+   console.log(e.target);
+   const {name, value, files} = e.target;
+  console.log(name, value, files);
+  
+
+  setUserDetails((prev)=>{
+      return {...prev, [name]:value}
+  })
+  
+  if(files && files[0]) setImage(URL.createObjectURL(files[0]))
+ 
+ }
+ console.log({userDetails});
+
+ 
+ const handleSubmit = (e) => {
+  e.preventDefault()
+  
+  console.log(userDetails)
+ }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstname">FirstName</label>
+          <input type="text" name='firstname' id='firstname' value={userDetails.firstname} onChange={handleChange}/>
+        </div>
+        <div>
+          <label htmlFor="firstname">LastName</label>
+          <input type="text" name='lastname' value={userDetails.lastname} id='lastname' onChange={handleChange}/>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name='email' value={userDetails.email} id='email' onChange={handleChange}/>
+        </div>
+        <div>
+          <label htmlFor="image">Upload Image</label>
+          <input type="file" name='src' onChange={handleChange}/>
+          {image && <img src={image } />}
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
